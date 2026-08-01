2d5df9519b7c43c51a41d402d050282f99986c77
Agustinadearagon <72619859+Agustinadearagon@users.noreply.github.com>
2026-08-02 01:42:07 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE = "rastro-digital-v1";
+
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./sw.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (k) {
+          return k !== CACHE;
+        }).map(function (k) {
+          return caches.delete(k);
+        })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    fetch(e.request)
+      .then(function (res) {
+        var copia = res.clone();
+        caches.open(CACHE).then(function (cache) {
+          cache.put(e.request, copia);
+        });
+        return res;
+      })
+      .catch(function () {
+        return caches.match(e.request);
+      })
+  );
+});
